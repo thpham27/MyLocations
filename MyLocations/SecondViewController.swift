5c4f97fc2564298a3c1da94c215a27458cb0982c
Thanh Pham <tpham27@live.spcollege.edu>
2020-09-14 14:44:10 -0400

    Initial Commit

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Thanh Pham on 9/14/20.
+//  Copyright © 2020 Thanh Pham. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
